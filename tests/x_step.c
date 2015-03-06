19a15a44f58f70d5288b9ff67d09466e612e9dbf
Joakim Børlum <joakim@roevhat.dk>
2015-03-06 13:09:40 +0100

    time_get error

--- tests/x_step.c
+++ tests/x_step.c
@@ -29,7 +29,7 @@ void *sampler(void *args) {
 
     RTIME t_pre = rt_get_time();
     while (1) {
-        printf("T = %f s;\n", (float)rt_get_time_ns()/1E9 - t_pre));
+        printf("T = %f s;\n", (float)rt_get_time_ns()/1E9 - t_pre);
         t_pre = (float)rt_get_time_ns()/1E9;
         rt_task_wait_period();
     }
