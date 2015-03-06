6bf73ba347fee9f28c6fc9fc09175930b5877b87
Joakim Børlum <joakim@roevhat.dk>
2015-03-06 13:25:31 +0100

    SIG HANDLE + file

--- tests/x_step.c
+++ tests/x_step.c
@@ -11,8 +11,18 @@ const int aref = AREF_GROUND;
 const char device_name[] = "/dev/comedi0";
 comedi_t *device;
 
+FILE *fp;
+
+int run = NULL;
+
 RT_TASK *rt_sampler;
 pthread_t thread_sampler;
+
+void sig_handler(int sig) {
+    run = NULL;
+    usleep(100 * 1000);
+    fclose(fp); 
+}
 
 void *sampler(void *args) {
     RTIME t_sample = nano2count(10 * TICK_TIME);
@@ -20,16 +30,15 @@ void *sampler(void *args) {
 
     int ID = nam2num("sampler");
     if (!(rt_sampler = rt_task_init_schmod(ID,1,0,0,SCHED_FIFO,0))) {
-        printf("ERROR: Could not init. task [TASK2]\n");
+        printf("ERROR: Could not init. task [sampler]\n");
         exit(1); 
     }
 
     rt_task_make_periodic(rt_sampler, t_expected, t_sample);
     rt_make_hard_real_time();
-
-    unsigned int t_pre = (unsigned int)rt_get_time_ns();
-    while (1) {
-        printf("T = %d ns;\n", (unsigned int)(rt_get_time_ns() - t_pre));
+    fp = fopen("data.csv", "w");
+    while (run) {
+        fprintf(fp, "T = %d ns;\n", (unsigned int)rt_get_time_ns());
         t_pre = (unsigned int)rt_get_time_ns();
         rt_task_wait_period();
     }
@@ -38,16 +47,18 @@ void *sampler(void *args) {
 }
 
 int main() {
+    signal(SIGINT, sig_handler);
     device = comedi_open(device_name);
     if (device == NULL) {
         printf("Error opening file, %s\n", device_name);
         exit(1);
     }
 
+    run = 1;
+
     pthread_create(&thread_sampler, NULL, &sampler, NULL);
 
-    printf("FARVEL!\n");
-    while (1) {
+    while (run) {
         usleep(100 * 1000);
     }
 
