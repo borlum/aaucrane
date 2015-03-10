212eb7f688bf2bc7ba9ad282e84822703052f401
Joakim Børlum <joakim@roevhat.dk>
2015-03-10 14:35:35 +0100

    Clean up

--- tests/data/plotStep.m
+++ tests/data/plotStep.m
@@ -6,13 +6,14 @@ function [] = plotStep(file)
 
     stepData = grabData(file);
 
-    plot(stepData.t / 1e9, stepData.x);
+    plot(stepData.t / 1e9, stepData.phi);
     xlabel('Time [s]');
     ylabel('Voltage [V]');
 
     function [data] = grabData(file)
         raw = csvread(file, 1, 0);
         data.t = raw(:,1);
+        data.phi = raw(:,2);
         data.x = raw(:,3);
     end
 end
