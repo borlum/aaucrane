ea3c558110b910678c97a7a8988425e2a0b067e1
Joakim Børlum <joakim@roevhat.dk>
2015-03-10 15:47:22 +0100

    No dio

--- examples/no_reg_magnet/no_reg_magnet.c
+++ examples/no_reg_magnet/no_reg_magnet.c
@@ -41,6 +41,7 @@ void control_magnet() {
   /*FLIP: 47, 49*/
 
   //IO SUBDEV: 10, 7, 2
+  comedi_dio_config(device, 10, 17, COMEDI_INPUT);
   comedi_data_read(device, 10, 17, 0, AREF_GROUND, &IN);
   printf("BUTTON READ: %d\n", IN);
   //comedi_dio_write(device, 1, 7, IN);
