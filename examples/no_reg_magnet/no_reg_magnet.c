cd1cc6c6861c4514b6c26d7c048ed6a0dda1f7b3
Lasse Bromose <lbromo12@student.aau.dk>
2015-03-17 18:34:27 +0100

    :)

--- examples/no_reg_magnet/no_reg_magnet.c
+++ examples/no_reg_magnet/no_reg_magnet.c
@@ -48,7 +48,7 @@ void control_magnet() {
   //IO SUBDEV: 10, 7, 2
   comedi_dio_read(device, DIGITAL_IO_SUBDEV, MAGNET_FLIP, &IN);
   printf("MAGNET_FLIP: %d\n", IN);
-  printfcomedi_dio_write(device, DIGITAL_IO_SUBDEV, MAGNET_ENABLE, 0);
+  printf(comedi_dio_write(device, DIGITAL_IO_SUBDEV, MAGNET_ENABLE, 0));
   //48 = 07
 }
 
