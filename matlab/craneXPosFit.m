026585febe37b31c5a600ee87f63baa096882858
Joakim Børlum <joakim@roevhat.dk>
2015-03-25 15:47:30 +0100

    Fit x and y voltage

---
+++ matlab/craneXPosFit.m
@@ -0,0 +1,28 @@
+% GRAB DATA FROM CRANE
+CRANE_URL = 'http://172.26.12.144/data';
+TMP_FILE  = 'tmp.csv';
+websave(TMP_FILE, [CRANE_URL '/crane/xsteps/1427291280.csv']);
+raw = csvread(TMP_FILE, 2, 0);
+
+% GRAB DATA FOR X POS. SENSOR
+Vx = raw(:,4);
+
+% GRAB ONLY LINEAR PART
+Vx = Vx(100:18177);
+
+x_lin = linspace(0, 4.03, length(Vx));
+
+[p, s] = polyfit(Vx, x_lin', 1);
+
+x_est = polyval(p, Vx);
+
+x = p(1) * Vx + p(2);
+
+r2 = 1 - s.normr^2 / norm(x_lin-mean(x_lin))^2
+
+% PLOT
+plot(Vx, x, Vx, x_lin);
+title('Position sensor, x');
+xlabel('Voltage [V]');
+ylabel('x [m]');
+grid on;
---
+++ matlab/craneYPosFit.m
@@ -0,0 +1,30 @@
+% GRAB DATA FROM CRANE
+CRANE_URL = 'http://172.26.12.144/data';
+TMP_FILE  = 'tmp.csv';
+websave(TMP_FILE, [CRANE_URL '/crane/xsteps/MANUAL_1427293912.csv']);
+raw = csvread(TMP_FILE, 1, 0);
+
+% GRAB DATA FOR X POS. SENSOR
+Vy = raw(:,5);
+
+% GRAB ONLY LINEAR PART
+Vy = Vy(1:6500);
+
+y_lin = linspace(0.02, 0.85, length(Vy));
+
+[p, s] = polyfit(Vy, y_lin', 1);
+
+p
+
+y_est = polyval(p, Vy);
+
+y = p(1) * Vy + p(2);
+
+r2 = 1 - s.normr^2 / norm(y_lin-mean(y_lin))^2
+
+% PLOT
+plot(Vy, y, Vy, y_lin);
+title('Position sensor, y');
+xlabel('Voltage [V]');
+ylabel('y [m]');
+grid on;
