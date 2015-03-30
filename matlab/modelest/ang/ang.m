b1249b3cd66aa4340125a27e8b801a1c09e3b8d4
Joakim Børlum <joakim@roevhat.dk>
2015-03-30 10:30:27 +0200

    Better parameter estimation

--- matlab/modelest/ang/ang.m
+++ matlab/modelest/ang/ang.m
@@ -1,7 +1,7 @@
 % XPOS MODEL ESTIMATION
 CRANE_URL = 'http://172.26.12.144/data';
 TMP_FILE  = 'tmp.csv';
-websave(TMP_FILE, [CRANE_URL '/crane/xsteps/1427464633.csv']);
+websave(TMP_FILE, [CRANE_URL '/crane/x_square/1427701730.csv']);
 
 DATA = csvread(TMP_FILE, 2, 0);
 t = DATA(:,1) * 1e-6;
@@ -11,8 +11,9 @@ idx_end = idx_end(1);
 t = t(1:idx_end);
 t = linspace(0,t(idx_end), idx_end);
 
-y = 2*(0.5279 * DATA(:,2) - 3.85);
+y = (DATA(:,3) - 1.85) * 0.31;
 y = y(1:idx_end);
+
 u = DATA(:,8) * 2;
 u = -u(1:idx_end);
 
--- matlab/modelest/cranemodel.m
+++ matlab/modelest/cranemodel.m
@@ -19,11 +19,16 @@ mp = 3.5;
 l  = 0.6;
 g  = 9.82;
 
-b = 0.25;
-Ip = 1.51;
+% EST. STEP
+%b = 0.25;
+%Ip = 1.51;
+
+% EST. SQUARE
+b = 2.5787;
+Ip = 1.4524;
 
 W = (-mp*l*s^2)/((Ip * mp*l^2)*s^2 + b*s+mp*g*l + mp*g*l);
 
 inner = feedback(6.6 * M * X, 1);
 
-step(feedback(0.02 * inner * W,1));
+complete = feedback(9 * inner*W, 1);
