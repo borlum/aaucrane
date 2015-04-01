80d0b7337358b956b639edc81a4a376f302b2148
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-01 15:33:31 +0200

    test

---
+++ tests/xpos_controller.c
@@ -0,0 +1,41 @@
+#include <libcrane.h>
+
+#define K_theta = 12
+#define K_p = 26.6
+
+int main(int argc,char* argv[]){
+
+  run_motorx(0);
+  run_motory(0);
+  
+  if(argc != 2){
+    printf("usage: %s <distance in meters>", argv[0]);
+  }
+  
+  double angle_ref = 0;
+  double angle_err = 0;
+
+  double x_ref = atof(argv[1]);
+  double x_err = 0;
+
+  double output = -1;
+  
+  while(output != 0){
+    angle_err = (angle_ref - get_angle()) * K_theta;
+
+    output = notch_filter(angle_err);
+
+    output = (output + x_ref - get_xpos()) * K_p;
+
+    if(output > 14)
+      output = 14;
+    else if (output < -14)
+      output = -14;
+
+    run_motorx(output);
+  }  
+}
+
+double notch_filter(double theta){
+  return theta;
+}
