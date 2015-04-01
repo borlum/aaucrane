91cf0ea64d3e186af6f3b5cf511688b9014e77c9
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-01 16:37:06 +0200

    crane

--- tests/xpos_controller.c
+++ tests/xpos_controller.c
@@ -6,15 +6,16 @@ static double K_p = 26.6;
 
 double notch_filter(double theta);
 
-int main(int argc,char* argv[]){
-
-  run_motorx(0);
-  run_motory(0);
-  
+int main(int argc,char* argv[]){  
   if(argc != 2){
     printf("usage: %s <distance in meters>", argv[0]);
     exit(1);
   }
+
+  initialize_crane();  
+  run_motorx(0);
+  run_motory(0);
+
   
   double x_ref = atof(argv[1]);
   double x_err = 0;
