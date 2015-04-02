df68f8d51d6950511ba021b97a6fb510f09682f2
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-02 11:31:08 +0200

    morten må ikke gå

--- tests/xpos_controller.c
+++ tests/xpos_controller.c
@@ -10,7 +10,7 @@ double notch_filter(double theta);
 
 int main(int argc,char* argv[]){  
   if(argc != 2){
-    printf("usage: %s <distance in meters>", argv[0]);
+    printf("usage: %s <x y>", argv[0]);
     exit(1);
   }
   
@@ -25,43 +25,53 @@ int main(int argc,char* argv[]){
   double x_ref = atof(argv[1]);
   double x_pos = 0;
 
-  double output = -1;
+  double x_ref = atof(argv[2]);
+  double y_pos = 0;
+  
+  double x_out = -1;
+  double y_out = -1;
 
   printf("Moving to x = %f\n", x_ref);
 
-  while(1){
-
-    //E[i % 3] = - get_angle() * 12;
-    //X[i % 3] = 0.2*E[i % 3] - 0.4*E[(i-1) % 3] + 0.2*E[(i-2) % 3] + 1.9*X[(i-1) % 3] - 0.9*X[(i-2) % 3];
-
-    
+  while(1){    
     x_pos = get_xpos();
-    output = (x_ref + 0.03 * get_angle() - x_pos) * K_p;
-
+    x_out = (x_ref - x_pos) * K_p;
+    
     if( (x_ref - 0.01) < x_pos && x_pos < (x_ref + 0.01) )
-      output = 0;
+      x_out = 0;
 
     printf("X_ref: %3f | Angle: %3f | X_err: %3f | Output: %3f\n", x_ref, 0.03 * get_angle(), x_pos, output);
 
     i++;
     
-    if(output == 0){
+    if(x_out == 0){
       run_motorx(0);
-      continue;
+      
+      y_out = (y_ref - get_ypos()) * 80;
+      run_motory(y_out);
+      if( (y_ref - 0.001) < x_pos && x_pos < (y_ref + 0.001) )
+	y_out = 0;
+
+      else if(-5 < y_out && y_out < 0)
+	y_out = -5;
+      else if (5 > y_out && y_out > 0)
+	y_out = 5;
+      run_motory(y_out);
+
     }
-    else if(output > 14)
-      output = 14;
-    else if (output < -14)
-      output = -14;
-    else if(-5 < output && output < 0)
-      output = -5;
-    else if (5 > output && output > 0)
-      output = 5;
+    else if(x_out > 14)
+      x_out = 14;
+    else if (x_out < -14)
+      x_out = -14;
+    else if(-5 < x_out && x_out < 0)
+      x_out = -5;
+    else if (5 > x_out && x_out > 0)
+      x_out = 5;
 
 
     printf("Readback voltage: %f\n", get_motorx_voltage());
 
-    run_motorx((int)output);
+    //run_motorx((int)output);
     
     usleep(1000 * 100);
   }  
