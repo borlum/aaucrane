f7f354bd27d9dbde8d48eb9601c74ef492d07f44
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-02 12:32:52 +0200

    forgot to add file

---
+++ manual/new_manual/manuel.c
@@ -0,0 +1,19 @@
+#include #include <pthread.h>
+#include <cranelib.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define DATA_PATH "/var/www/html/data/crane/ysteps/"
+#define DATA_HEADER "TIME,ANGLE1,ANGLE2,XPOS,YPOS,XTACHO,YTACHO,XVOLT,YVOLT\n"
+
+int main(int argc, char* argv[])
+{
+  initialize_crane();
+  
+  while(1){
+    printf("X_pos: %3f | Y_pos: %3f | ANGLE: ", get_xpos(), get_ypos(), get_angle());
+    run_motorx(get_ctrlpad_x());
+    run_motory(get_ctrlpad_y());
+    usleep(1000 * 100);
+  }
+}
