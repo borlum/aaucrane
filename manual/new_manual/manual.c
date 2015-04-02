e1bf698be30952f00dc974ef7373987674c765ae
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-02 12:36:33 +0200

    .

--- manual/new_manual/manual.c
+++ manual/new_manual/manual.c
@@ -11,6 +11,7 @@ int main(int argc, char* argv[])
   initialize_crane();
   
   while(1){
+    printf("JOYPAD: (x,y) = (%3f, %3f)", get_ctrlpad_x(), get_ctrlpad_y());
     printf("X_pos: %3f | Y_pos: %3f | ANGLE: ", get_xpos(), get_ypos(), get_angle());
     run_motorx(get_ctrlpad_x());
     run_motory(get_ctrlpad_y());
