6c1aa8a6a807635265b38d61564a0fa8c760604b
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-03 08:51:47 +0200

    .

--- acc/acc.c
+++ acc/acc.c
@@ -201,12 +201,13 @@ void *controller(void * args)
   }
   usleep(1000 * 5000);
   /* Move to start (0,0) */
+  /*
   double nul = 0.50;
   mq_send(output_x, (char*) &nul, sizeof(double), 0);
   mq_receive(input, input_buffer, MSG_SIZE, 0);
   mq_send(output_y, (char*) &nul, sizeof(double), 0);
   mq_receive(input, input_buffer, MSG_SIZE, 0);
-
+  */
 
   /* Power down */
 #ifndef TEST
