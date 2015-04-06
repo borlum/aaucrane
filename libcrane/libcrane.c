5e4a4e556e639d7b02c0d1c81bee548c986f5043
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-06 13:55:10 +0200

    .

--- libcrane/libcrane.c
+++ libcrane/libcrane.c
@@ -249,7 +249,7 @@ double get_ctrlpad_x()
 
   new_val = -( (((old_val - old_min) * new_range) / old_range) + new_min );
 
-  return new_val - 1.5;
+  return new_val - 0.5;
 }
 /**
  * Samples current y-axis voltage of control pad
