aaa37232412430aff4a6831ac91f63a7b567fe2f
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-06 15:15:14 +0200

    gain

--- manual/new_manual/manual.c
+++ manual/new_manual/manual.c
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
   
   while(1){
 #ifdef ANGLE
-    printf("Raw measurement: %f | Converted to rad: %f | Converted to deg: %f\n", get_angle_raw(), get_angle(), get_angle() * (180/3.14));
+    printf("Raw measurement: %f | Converted to rad: %f | Converted to deg: %f\n", get_angle_raw(), get_angle(), get_angle() * (180.0/3.14));
 #else
     printf("JOYPAD: (x,y) = (%3f, %3f)\n", get_ctrlpad_x(), get_ctrlpad_y());
     printf("X_pos: %3f @ %3f| Y_pos: %3f @ %3f | ANGLE: %3f\n", get_xpos(), get_xpos_raw(), get_ypos(), get_ypos_raw(), get_angle());
