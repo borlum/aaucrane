f2d508f72cb142a5bec7faafddf54841e2c43eb3
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-06 13:47:34 +0200

    .

--- libcrane/libcrane.c
+++ libcrane/libcrane.c
@@ -241,10 +241,10 @@ double get_ctrlpad_x()
 
   printf("RAW_VAL: %f\n", raw_val);
   
-  old_val = raw_val;
+  old_val = raw_val - 0.9;
   
   old_max =  10; new_max   =  14;
-  old_min = 0.6;   new_min   = -14;
+  old_min = 0;   new_min   = -14;
 
   old_range = old_max - old_min;
   new_range = new_max - new_min;
