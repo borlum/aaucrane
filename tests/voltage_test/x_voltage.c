03dd2b656ffabffcef075fe3d36667d04951fbb3
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-06 13:35:59 +0200

    .

--- tests/voltage_test/x_voltage.c
+++ tests/voltage_test/x_voltage.c
@@ -60,7 +60,7 @@ int main(){
 
   for(int output = 0; output <= 4200; output += 200){
     comedi_data_write(NI_card, AOUT_SUBDEV, 0, 0, AREF_GROUND, output);
-    printf("%d,%f\n", output, (get_sensor_raw(CHAN_XIN_IN) * 2));
     usleep(1000 * 500);
+    printf("%d,%f\n", output, (get_sensor_raw(CHAN_XIN_IN) * 2));
   }
 }
