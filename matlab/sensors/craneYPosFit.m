33e8caa24edd05991802bd8f990d6e6b1b778bab
Sgammeljord <mix_606_@msn.com>
2015-04-06 16:41:02 +0200

    mmmm

--- matlab/sensors/craneYPosFit.m
+++ matlab/sensors/craneYPosFit.m
@@ -1,7 +1,7 @@
 % GRAB DATA FROM CRANE
 CRANE_URL = 'http://172.26.12.144/data';
 TMP_FILE  = 'tmp.csv';
-websave(TMP_FILE, [CRANE_URL '/crane/xsteps/MANUAL_1427293912.csv']);
+websave(TMP_FILE, [CRANE_URL '/crane/manual/MANUAL_1427293912.csv']);
 raw = csvread(TMP_FILE, 1, 0);
 
 % GRAB DATA FOR X POS. SENSOR
