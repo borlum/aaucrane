1f832d0e2c4c2f688fde6ec045ca8b61e2095494
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-06 13:40:14 +0200

    .

--- libcrane/libcrane.c
+++ libcrane/libcrane.c
@@ -238,13 +238,11 @@ double get_ctrlpad_x()
   double old_range, new_range, old_max, old_min, new_max, new_min;
 
   raw_val = get_sensor_raw(CHAN_CTRLPAD_X_IN);
-
-  printf("get_ctrlpad_x RAW VALUE: %f\n", raw_val);
   
   old_val = raw_val;
   
   old_max =  10; new_max   =  14;
-  old_min = 0;   new_min   = -14;
+  old_min = 0.5;   new_min   = -14;
 
   old_range = old_max - old_min;
   new_range = new_max - new_min;
