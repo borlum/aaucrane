078a5787c453346666b4c09b76c61f05a5374958
Joakim Børlum <joakim@roevhat.dk>
2015-04-06 14:07:43 +0200

    RAW XPOS

--- manual/new_manual/manual.c
+++ manual/new_manual/manual.c
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
   
   while(1){
     printf("JOYPAD: (x,y) = (%3f, %3f)\n", get_ctrlpad_x(), get_ctrlpad_y());
-    printf("X_pos: %3f | Y_pos: %3f at %3f | ANGLE: %3f\n", get_xpos(), get_ypos(), get_ypos_raw(), get_angle());
+    printf("X_pos: %3f @ %3f| Y_pos: %3f @ %3f | ANGLE: %3f\n", get_xpos(), get_xpos_raw(), get_ypos(), get_ypos_raw(), get_angle());
     run_motorx(get_ctrlpad_x());
     run_motory(get_ctrlpad_y());
 
