03ed54fe5a14d020058affffd3f24dd9bc24efaa
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-07 15:32:57 +0200

    [Grid] MEGA COMMIT..

---
+++ acc/src/stack.c
@@ -0,0 +1,29 @@
+#include "stack.h"
+
+stack_loc_t stack[STACK_WIDTH][STACK_HEIGHT];
+
+void init_stack(){
+  for(int i = 0; i < STACK_WIDTH; i++){
+    for(int j = 0; j < STACK_HEIGHT; j++){
+      double x = (i * CONTAINER_HEIGHT) + X_OFF_SET;
+      double y = Y_OFF_SET - (j * CONTAINER_HEIGHT);
+      stack[i][j].loc.x = x;
+      stack[i][j].loc.y = y;
+      stack[i][j].status = FREE;
+    }
+  }
+}
+
+stack_loc_t get_stack_loc(int i, int j){
+  return stack[i][j];
+}
+
+stack_loc_t update_status(int i, int j, loc_status_t status){
+  stack[i][j].status = status;
+  return stack[i][j];
+}
+
+
+loc_status_t get_status(int i, int j){
+  return stack[i][j].status;
+}
