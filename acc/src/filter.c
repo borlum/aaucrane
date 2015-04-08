d3b7d88aa449eeab37c3dd71401677610038dad2
Joakim Børlum <joakim@roevhat.dk>
2015-04-08 20:27:25 +0200

    New gains, as according to simulations

--- acc/src/filter.c
+++ acc/src/filter.c
@@ -1,7 +1,7 @@
 #include "../include/filter.h"
 
 double angle_controller(double error){
-  double k_p = 0.5;
+  double k_p = 2;
   return error * k_p;
 }
 
@@ -11,7 +11,7 @@ double position_controller_x(double error){
 }
 
 double position_controller_y(double error){
-  double k_p = 80;
+  double k_p = 49.3;
   return error * k_p;
 }
 
