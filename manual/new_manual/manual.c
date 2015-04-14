af58a7bb51cb19be42e821f7eda6b725e6d298b6
Joakim Børlum <joakim@roevhat.dk>
2015-04-14 13:50:50 +0200

    Manual

--- manual/new_manual/manual.c
+++ manual/new_manual/manual.c
@@ -2,10 +2,22 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define DATA_PATH "/var/www/html/data/crane/manual/"
+#define DATA_HEADER "TIME,ANGLE1,ANGLE2,XPOS,YPOS,XTACHO,YTACHO,XVOLT,YVOLT\n"
+
 int main(int argc, char* argv[])
 {
   initialize_crane();
-  
+
+  /*PREPARE FILE*/
+  char tmp[160];
+  sprintf(tmp, "%s/%d.csv", DATA_PATH, (int)time(NULL));
+  fp = fopen(tmp, "w");
+  fprintf(fp, DATA_HEADER);
+
+  unsigned long t_0, t_sample;
+
+  t_0 = get_time_micros();
   while(1){
 #ifdef ANGLE
     printf("Raw measurement: %f | Converted to rad: %f | Converted to deg: %f\n", get_angle_raw(), get_angle(), get_angle() * (180.0/3.14));
@@ -21,6 +33,23 @@ int main(int argc, char* argv[])
         disable_magnet();
     }
 #endif
+
+    /*GRAB TIMESTAMP*/
+    t_sample = get_time_micros();
+    fprintf(fp, "%ld,",  (t_sample - t_0));
+
+    /*SAMPLE SENSORS*/
+    fprintf(fp, "%f,", get_old_angle_raw());
+    fprintf(fp, "%f,", get_angle_raw());
+    fprintf(fp, "%f,", get_xpos_raw());
+    fprintf(fp, "%f,", get_ypos_raw());
+    fprintf(fp, "%f,", get_motorx_velocity_raw());
+    fprintf(fp, "%f,", get_motory_velocity_raw());
+    fprintf(fp, "%f,", get_motorx_voltage());
+    fprintf(fp, "%f,", get_motory_voltage());
+    fprintf(fp, "\n");
+
+
     usleep(1000 * 100);
   }
 }
