5d9c4da29e6a046a45dd0562bbc918df826d0a58
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-15 09:45:54 +0200

    step ready

--- libcrane/libcrane.c
+++ libcrane/libcrane.c
@@ -6,7 +6,7 @@ comedi_t *NI_card;
 int *NI_card;
 #endif
 
-static const double MAX_MOTOR_OUTPUT = 13;
+static const double MAX_MOTOR_OUTPUT = 12.5;
 static const double MIN_MOTOR_OUTPUT = 4.5;
 static double ZERO_ANGLE_RAD;
 
