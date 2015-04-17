0fa2376093c5ada234c8f0feb697964a36dd4ae8
DisasterMaster <mortenrechter@hotmail.com>
2015-04-17 13:06:00 +0200

    Ny velo controller implement

--- acc/include/filter.h
+++ acc/include/filter.h
@@ -4,5 +4,6 @@
 double angle_controller(double error);
 double position_controller_x(double error);
 double position_controller_y(double error);
+double velocity_controller_x(double error);
 
 #endif
