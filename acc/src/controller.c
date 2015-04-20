9a18bde523995babd5d8b10167f93ec57ddad46c
DisasterMaster <mortenrechter@hotmail.com>
2015-04-20 13:49:50 +0200

    Output print

--- acc/src/controller.c
+++ acc/src/controller.c
@@ -71,7 +71,7 @@ void *task_x_axies_controller(void * argc)
     printf("[angle_err] out: %lf\n", angle_err);
     x_err = x_ref - x_pos - angle_controller(angle_err);
     printf("[X_err] out: %lf\n", x_err);
-    out = position_controller_x(x_err) + pI * x_err_int;
+    out = position_controller_x(x_err) - pI * x_err_int;
     velocity_err = out - get_motorx_velocity();
     printf("[velocity_ref] out: %lf\n", out);
     out = velocity_controller_x(velocity_err);
