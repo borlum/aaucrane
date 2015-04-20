598eae6a19d14fc70dc71f96f367bb8945c57df8
DisasterMaster <mortenrechter@hotmail.com>
2015-04-20 13:05:11 +0200

    ny negativ saturation

--- libcrane/libcrane.c
+++ libcrane/libcrane.c
@@ -40,12 +40,12 @@ int initialize_crane()
  */
 int run_motorx(double voltage)
 {
-    if (voltage > -6 && voltage < 0 - epsilon) {
-        voltage = -6;
+    if (voltage > -4.5 && voltage < 0 - epsilon) {
+        voltage = -4.5;
     }
 
-    if (voltage < 4.5 && voltage > 0 + epsilon) {
-        voltage = 4.5;
+    if (voltage < 6 && voltage > 0 + epsilon) {
+        voltage = 6;
     }
 
     return run_motor(-voltage, 0); /* Change X motor direction */
