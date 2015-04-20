426232e02b96a8308a75c9d3c73995c0c92145ab
DisasterMaster <mortenrechter@hotmail.com>
2015-04-20 15:32:44 +0200

    debug

--- libcrane/libcrane.c
+++ libcrane/libcrane.c
@@ -44,8 +44,8 @@ int run_motorx(double voltage)
         voltage = -4.5;
     }
 
-    if (voltage < 4.5 && voltage > 0 + epsilon) {
-        voltage = 4.5;
+    if (voltage < 5 && voltage > 0 + epsilon) {
+        voltage = 5;
     }
 
     return run_motor(-voltage, 0); /* Change X motor direction */
