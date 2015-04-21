d0f8ebc6a3c1d328a8d8b2ae59ca07a00d42220f
DisasterMaster <mortenrechter@hotmail.com>
2015-04-21 14:54:50 +0200

    xpos controller

--- acc/src/controller.c
+++ acc/src/controller.c
@@ -87,7 +87,7 @@ void *task_x_axies_controller(void * argc)
     x_err_int += x_err;
 
     if(fabs(x_err_int) >= 2 ){
-      x_err_int > 0 ? 2 : -2;
+      x_err_int = (x_err_int > 0 ? 2 : -2);
 
     }
 
