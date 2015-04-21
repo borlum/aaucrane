5d3b2143a080ebbe87fdb91d1679437769263b06
DisasterMaster <mortenrechter@hotmail.com>
2015-04-21 17:02:29 +0200

    position controller

--- acc/src/controller.c
+++ acc/src/controller.c
@@ -86,6 +86,7 @@ void *task_x_axies_controller(void * argc)
     angle_err = angle_ref - angle_pos;
     printf("[angle] out: %.2lf\n", angle_err);
     x_err = x_ref - x_pos - angle_controller(angle_err);
+    printf("[X_ref] out: %.2lf\n", x_ref);
     printf("[X_pos] out: %.2lf\n", x_pos);
     printf("[X_vel] out: %lf\n", velocity);
     out = position_controller_x(x_err) + pI * x_err_int * .001 - velocity_controller_x(velocity);
