f74f9e7771ae80c005bd04975ad7b105fbd1a9aa
DisasterMaster <mortenrechter@hotmail.com>
2015-04-21 14:29:18 +0200

    epsilon på x velo

--- acc/src/filter.c
+++ acc/src/filter.c
@@ -4,14 +4,14 @@
 const static double TS = 0.001;
 
 double error_sum = 0;
-double old_error = 0; 
+double old_error = 0;
 
 double angle_controller(double error){
   const double k_p = 45.8;
   const double k_i = 1;
   const double k_d = 10;
 
-  
+
   double out;
 
   out = k_p * error + k_i * error_sum * TS + k_d * (error - old_error) / TS;
@@ -27,12 +27,12 @@ double angle_controller(double error){
   printf("I: %lf\n", k_i * error_sum * TS);
   printf("D: %lf\n", k_d * (error - old_error) / TS);
   printf("PID: %lf\n", out);
-  
+
   return out;
 }
 
 double position_controller_x(double error){
-  double k_p = 60;
+  double k_p = 25;
   return error * k_p;
 }
 
