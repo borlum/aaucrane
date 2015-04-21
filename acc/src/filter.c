3e13cdc961e3c766d160e003a3a25c20b5b10ca3
DisasterMaster <mortenrechter@hotmail.com>
2015-04-21 10:14:06 +0200

    pid

--- acc/src/filter.c
+++ acc/src/filter.c
@@ -1,13 +1,19 @@
 #include "../include/filter.h"
-
+double error_sum = 0, error_old = 0;
 
 double angle_controller(double error){
-  double k_p = 5;
-  return error * k_p;
+  double k_p = 45.8;
+  double k_i = 1; k_d = 10;
+   double out;
+
+  out = error * (k_p + k_d * (error-error_old)*1/1000 + k_i * error_sum);
+  error_old = error;
+  error_sum += error;
+  return out;
 }
 
 double position_controller_x(double error){
-  double k_p = 150;
+  double k_p = 60;
   return error * k_p;
 }
 
@@ -17,6 +23,6 @@ double position_controller_y(double error){
 }
 
 double velocity_controller_x(double error){
-  double k_p = 1;
+  double k_p = 50;
   return k_p * error;
 }
