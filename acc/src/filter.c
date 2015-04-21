0e5b0eba7044b24fe6758602d20a7fa3179d44d8
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-21 13:42:13 +0200

    PID

--- acc/src/filter.c
+++ acc/src/filter.c
@@ -14,9 +14,9 @@ double angle_controller(double error){
   
   double out;
 
-  out = k_p * error + k_i * error_sum + k_d * (error - old_error) * TS;
+  out = k_p * error + k_i * error_sum + k_d * (error - old_error);
 
-  error_sum += error * TS;
+  error_sum += error;
   old_error = error;
 
   printf("#### PID ####\n");
