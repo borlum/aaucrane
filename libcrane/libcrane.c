79b6281441d20d8b0bd7acdef95d7b20c4d6ee42
DisasterMaster <mortenrechter@hotmail.com>
2015-04-21 14:17:39 +0200

    epsilon på x velo

--- libcrane/libcrane.c
+++ libcrane/libcrane.c
@@ -219,7 +219,13 @@ double get_motorx_velocity()
  */
 double get_motorx_velocity_raw()
 {
-    return get_sensor_raw(CHAN_XVEL_IN)-.115;
+    double D;
+    D = get_sensor_raw(CHAN_XVEL_IN)-.115+.0077
+    if (D < 0.001){
+      D = 0;
+    }
+
+    return D;
 }
 
 /**
