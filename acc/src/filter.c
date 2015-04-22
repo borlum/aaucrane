49a790c1622975cac3a10640d08364b8a4a6a33c
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-22 13:05:53 +0200

    more K_d

--- acc/src/filter.c
+++ acc/src/filter.c
@@ -9,7 +9,7 @@ double old_error = 0;
 double angle_controller(double error){
   const double k_p = 50;
   const double k_i = 0;
-  const double k_d = .1;
+  const double k_d = .2;
 
 
   double p = 0, i = 0, d = 0, out = 0;
