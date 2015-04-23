7be94bfb4311b71852874fe846a466da2c11edd3
DisasterMaster <mortenrechter@hotmail.com>
2015-04-23 13:39:00 +0200

    hit count = 100

--- acc/src/controller.c
+++ acc/src/controller.c
@@ -70,7 +70,7 @@ void *task_x_axies_controller(void * argc)
 
     if ( (fabs(x_ref-get_xpos()) < X_ERR_BAND) && (get_motorx_velocity() == 0) && (get_angle() == 0) ) {
       printf("X-POS: %lf\n", get_xpos());
-      if( (hit_count++) == 10 ){
+      if( (hit_count++) == 100 ){
 	run_motorx(0);
 	new_ref = 0;
 	hit_count = 0;
