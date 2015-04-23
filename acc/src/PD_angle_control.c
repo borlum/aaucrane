cf34f1b54b3e8608e179501165b8689633c630d4
DisasterMaster <mortenrechter@hotmail.com>
2015-04-23 11:19:55 +0200

    Angle control fil tilføjet

---
+++ acc/src/PD_angle_control.c
@@ -0,0 +1,30 @@
+
+#ifndef TEST
+#include <libcrane.h>
+#endif
+
+#include <filter.h>
+
+
+size_t ref_arr_sz = 8000, nr_of_ref;
+double ref_arr[ref_arr_sz];
+int current_index = 0;
+
+void pd_init_controller(double x_ref){
+  double step;
+  step = x_ref-get_xpos();
+  nr_of_ref = ramp_maker(step, ref_arr);
+  current_index = 0;
+}
+
+double pd_get_controller_output(){
+  double angle_pos = 0, out;
+  angle_pos = get_angle();
+  out = angle_controller(angle_pos);
+  out += position_controller_x(ref_arr[current_index] - get_xpos());
+  if(current_index < (nr_of_ref)) {
+    current_index++;
+  }
+
+  return(out);
+}
