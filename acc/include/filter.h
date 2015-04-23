dec545514d3e92ccc2e10d47910290c1f6a8bf9f
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-23 16:10:16 +0200

    fix header files

--- acc/include/filter.h
+++ acc/include/filter.h
@@ -1,7 +1,7 @@
 #ifndef _FILTER_H_
 #define _FILTER_H
 
-double angle_controller(double error);
+double angle_controller(double angle_pos);
 double position_controller_x(double error);
 double position_controller_y(double error);
 double velocity_controller_x(double error);
