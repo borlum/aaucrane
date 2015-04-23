9d543d98245c5c73e6f83a7608f928175815ea3d
DisasterMaster <mortenrechter@hotmail.com>
2015-04-23 13:19:05 +0200

    Steffan PID

--- acc/src/PD_angle_control.c
+++ acc/src/PD_angle_control.c
@@ -47,7 +47,7 @@ double pid_get_controller_output(){
 
   out += C2 * (ref_arr[current_index] - get_xpos());
 
-  out += pos_integrate;
+  out += pos_integrate*.5;
 
   pos_integrate += (ref_arr[current_index] - get_xpos()) * TS;
 
