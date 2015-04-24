6c2a74adf2e91a1e911f309e412359397e91b495
DisasterMaster <mortenrechter@hotmail.com>
2015-04-24 13:50:06 +0200

    Y-controller

--- acc/src/filter.c
+++ acc/src/filter.c
@@ -45,8 +45,8 @@ double position_controller_x2(double error){
 double position_controller_y(double error){
   double k_p = 49.3, comp;
 
-  if(error > 0) comp = -5;
-  else if(error < 0) comp = 10;
+  if(error > 0) comp = 0;
+  else if(error < 0) comp = 0;
 
   return error * (k_p + comp);
 }
