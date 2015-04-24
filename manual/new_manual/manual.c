7b1ad2eacd1aa259584cc150b1a92a7995fc824e
Lasse Bromose <lbromo12@student.aau.dk>
2015-04-24 13:33:56 +0200

    dont run motor

--- manual/new_manual/manual.c
+++ manual/new_manual/manual.c
@@ -39,8 +39,8 @@ int main(int argc, char* argv[])
     if( fabs((manual_y = get_ctrlpad_y())) < 2 )
       manual_y = 0;
     
-    run_motorx(manual_x + angle);
-    run_motory(manual_y);
+    //run_motorx(manual_x + angle);
+    //run_motory(manual_y);
 
     if (get_ctrlpad_magnet_switch()) {
         enable_magnet();
