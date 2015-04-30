f194c3da799d5564dcd20fdbe4e58eefde0db33a
DisasterMaster <mortenrechter@hotmail.com>
2015-04-30 15:05:08 +0200

    offset debug

--- libcrane/libcrane.c
+++ libcrane/libcrane.c
@@ -49,11 +49,13 @@ int run_motorx(double voltage)
         voltage = 4.2;
     }*/
 
+    if (voltage < 0.2 && voltage > -0.2) voltage = 0;
+
     if(voltage < 0) sign = -1;
     else if (voltage > 0) sign = 1;
     else if (voltage == 0) sign = 0;
 
-    voltage = sign * (sign * voltage + 4);
+    voltage = sign * (sign * voltage + 4.2);
 
     return run_motor(-voltage, 0); /* Change X motor direction */
 }
