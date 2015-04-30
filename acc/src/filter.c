90424444efe68b310128a5427ed592f55b6a77fd
DisasterMaster <mortenrechter@hotmail.com>
2015-04-30 17:35:37 +0200

    print ramp

--- acc/src/filter.c
+++ acc/src/filter.c
@@ -99,6 +99,7 @@ int ramp_maker(double step, double *ramp_arr){
     for(i = 0; i<step; i += speed){
       ramp_arr[j] = i + off_set;
       j++;
+      printf("ramp: %lf \n", ramp_arr[j]);
     }
   } else if(step < 0){
     for(i = 0; i>step; i -= speed){
