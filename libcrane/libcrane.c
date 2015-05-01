b6ffe22f57e6d5ca4168614d8e014128c4539ecb
DisasterMaster <mortenrechter@hotmail.com>
2015-05-01 10:35:50 +0200

    debug

--- libcrane/libcrane.c
+++ libcrane/libcrane.c
@@ -49,7 +49,7 @@ int run_motorx(double voltage)
         else if (voltage > 0) sign = 1;
         else if (voltage == 0) sign = 0;
 
-        voltage = sign * (sign * voltage + (4.2+(2-get_xpos())*0.07));
+        voltage = sign * (sign * voltage + 4.2) ; //+(2-get_xpos())*0.07));
 
     }
 
