0111eba17439ef6deaff6f838eee2b79b9af9304
Joakim Børlum <joakim@roevhat.dk>
2015-05-01 14:11:57 +0200

    Rename

--- acc/include/controller.h
+++ acc/include/controller.h
@@ -7,8 +7,8 @@
 #define SETTLE_HITS 500
 
 #ifdef RTAI
-extern RT_TASK *rt_x_axies_controller;
-extern RT_TASK *rt_y_axies_controller;
+extern RT_TASK *rt_x_axis_controller;
+extern RT_TASK *rt_y_axis_controller;
 
 #define X_SAMPLE_TIME_NS 1E6
 #define X_SAMPLE_TIME_S 1E-3 
@@ -17,7 +17,7 @@ extern RT_TASK *rt_y_axies_controller;
 
 #endif
 
-void* task_x_axies_controller(void *);
-void* task_y_axies_controller(void *);
+void* task_x_axis_controller(void *);
+void* task_y_axis_controller(void *);
 
 #endif
