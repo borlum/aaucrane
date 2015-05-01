be4ddbd3c626c6454cafe240f752142392a563f5
DisasterMaster <mortenrechter@hotmail.com>
2015-05-01 10:47:49 +0200

    mod til monitor

--- tests/monitor/monitor.c
+++ tests/monitor/monitor.c
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
     initialize_crane();
     
     while(1) {
-        printf("(x,y,\u03B8) = (%lf, %lf, %lf)\n", get_xpos(), get_ypos(), get_angle());
-        usleep(1000 * 1);
+        printf("(x,y,\u03B8) = (%.2lf, %.2lf, %.2lf)\n", get_xpos(), get_ypos(), get_angle());
+        usleep(1000000 * 1);
     }
 }
