85f69348195b1c5526043372c79acd09721f1a2c
Lasse Bromose <lbromo12@student.aau.dk>
2015-05-04 13:29:37 +0200

    trunk

--- acc/src/controller.c
+++ acc/src/controller.c
@@ -92,7 +92,7 @@ void *task_x_axis_controller(void * argc)
         /*Send msg that we have settled!*/
         int msg = 1;
         printf("[X]: DONE @ %lf\n", get_xpos());
-	printf("[X] trunked: %lf\n", ((double)(int)( (get_xpos()) * 10) / 10.00));
+	printf("[X]: trunked: %lf\n", ((double)(int)( (get_xpos()) * 100) / 100.00));
         if (mq_send(output, (char *)&msg, sizeof(int), 0) == -1)
           printf("%s\n", strerror(errno));
       }
