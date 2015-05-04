ce7c877b3fc476cbe377d98edd022c106f4df6b3
Lasse Bromose <lbromo12@student.aau.dk>
2015-05-04 13:13:27 +0200

    x debug

--- acc/src/controller.c
+++ acc/src/controller.c
@@ -84,6 +84,7 @@ void *task_x_axis_controller(void * argc)
     
     /*Settled?*/
     double err = x_ref - get_xpos();
+    printf("[X] err: %lf\n", err);
     /*X inside error band? Angle inside error band? Velocity = 0?*/
     if ( (fabs(err) < X_ERR_BAND) && (get_motorx_velocity() == 0) && (fabs(get_angle()) < ANGLE_ERR_BAND) ) {
       /*Has this happened more than SETTLE_HITS times?*/
