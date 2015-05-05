a3ccf2319f1e584c33c5ae9c4fcd9c6a91f14c77
DisasterMaster <mortenrechter@hotmail.com>
2015-05-05 16:50:24 +0200

    No sensor hack

--- libcrane/libcrane.c
+++ libcrane/libcrane.c
@@ -151,7 +151,7 @@ double get_angle()
 
     double ang = 0.7367*get_angle_raw() - offset;
 
-    if(ang_prev == ang) count++;
+ /*   if(ang_prev == ang) count++;
     else count = 0;
 
     if(count > 10) {
@@ -160,7 +160,7 @@ double get_angle()
     }
 
     ang_prev = ang;
-
+*/
     return libcrane_truncate(ang);
 }
 
