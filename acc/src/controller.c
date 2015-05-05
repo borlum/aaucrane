ac2593a33ab0f8c720fed00c6b2def9e5f5a0bc8
Lasse Bromose <lbromo12@student.aau.dk>
2015-05-05 21:06:15 +0200

    logger in step

--- acc/src/controller.c
+++ acc/src/controller.c
@@ -230,6 +230,7 @@ int init_logger(const char *data_path){
   _enable_logger = 0;
   _new_log = 1;
   sem_init(&_logger_sem, 0, 1);
+  printf("Malloc size:" sizeof(data_path) / sizeof(data_path[0]));
   _data_path = malloc( sizeof(data_path) / sizeof(data_path[0]) );
   memcpy(_data_path, data_path, (sizeof(data_path) / sizeof(data_path[0])));
 }
