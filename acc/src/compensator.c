da35e533d5ac625a3873a592443e1d3054341482
Joakim Børlum <joakim@roevhat.dk>
2015-05-06 10:04:29 +0200

    Tuning

--- acc/src/compensator.c
+++ acc/src/compensator.c
@@ -15,7 +15,7 @@ double angle_controller(double error){
   double out, k, td;
 
   /* After Kirsten */
-  k  = 10.0;
+  k  = 5.0;
   td =  1.0;
 
   //out =  k*td * ( ( (1/td) * (error - pre_error) / SAMPLE_TIME_S ) + 1);
