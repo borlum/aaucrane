7bbd0a25abeda5a32852e1f62f1adc0225adfb6a
Lasse Bromose <lbromo12@student.aau.dk>
2015-05-06 18:04:47 +0200

    more and less

--- acc/src/compensator.c
+++ acc/src/compensator.c
@@ -40,7 +40,7 @@ double angle_controller(double error){
   /* After Kirsten */  
   k  =  7.5;
   tp =  1;
-  td =  0.1;
+  td =  0.01;
 
   printf("=====================\n");
   printf("[C2] pre_error = %lf \n", pre_error);
@@ -66,7 +66,7 @@ double position_controller_x(double error){
 #ifdef NEW
   double k_p = 4;
 #else
-  double k_p = 4.75;
+  double k_p = 3.75;
 #endif
   /*printf("[C1] error = %lf \n", error);*/
  /* printf("=====================\n");
