24dad668bca31c801e9b2354c0a11e52f1eeb666
Joakim Børlum <joakim@roevhat.dk>
2015-05-06 14:36:31 +0200

    sample tid tilbage

--- acc/src/compensator.c
+++ acc/src/compensator.c
@@ -24,9 +24,9 @@ double angle_controller(double error){
   printf("[C2] pre_error = %lf \n", pre_error);
   printf("[C2] error     = %lf \n", error);
   printf("[C2] P         = %lf \n", k*tp * error);
-  printf("[C2] D         = %lf \n", k*td * (error - pre_error) / SAMPLE_TIME_S);
+  printf("[C2] D         = %lf \n", k*td * (error - pre_error) / 0.01);
 
-  out = k*tp * error + k*td * (error - pre_error) / SAMPLE_TIME_S;
+  out = k*tp * error + k*td * (error - pre_error) / 0.01;
   //out = error*240 - 160 * pre_error - pre_out;
 
   printf("[C2] OUT   = %lf \n", out);
