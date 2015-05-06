636644e292684246a09c7e8fd8d959d15b3cbd8b
Joakim Børlum <joakim@roevhat.dk>
2015-05-06 14:16:41 +0200

    CRAZY

--- acc/src/compensator.c
+++ acc/src/compensator.c
@@ -43,7 +43,7 @@ double angle_controller(double error){
 
 double position_controller_x(double error){
   /*W. container*/
-  double k_p = 3.75;
+  double k_p = 5.0;
 
   /*printf("[C1] error = %lf \n", error);*/
   printf("=====================\n");
