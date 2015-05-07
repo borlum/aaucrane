66f78bebfbbc45f9a4d18efafe8419534f34a877
Lasse Bromose <lbromo12@student.aau.dk>
2015-05-07 16:13:51 +0200

    d

--- tests/simple_controller/simple.c
+++ tests/simple_controller/simple.c
@@ -29,7 +29,9 @@ void *simple_controller(void *arg){
   double angle_err, pos_err;
   double angle_out, out;
 
-  double angle_kp = 1;
+  double prev_angle_err = 0;
+  
+  double angle_kp = 1, angle_kd = 0.02;
   double pos_kp = 10;
 
   printf("REF: %lf\n", pos_ref);
@@ -38,7 +40,7 @@ void *simple_controller(void *arg){
     angle_err = angle_ref - get_angle();
     pos_err = pos_ref - get_xpos();
     
-    angle_out = angle_kp * angle_err;
+    angle_out = angle_kp * angle_err + angle_kd * ((anglre_err - prev_angle_err) / 0.01);
     out = pos_kp * (pos_err - angle_out);
     
     printf("===================\n");
@@ -49,6 +51,8 @@ void *simple_controller(void *arg){
     printf("===================\n");
 
     run_motorx(out);
+
+    prev_angle_err = angle_err;
     
     rt_task_wait_period();
   }
