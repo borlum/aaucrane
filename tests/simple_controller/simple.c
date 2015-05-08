566da82b0cab0dfbf98f0fd50dac81bb83bbdb1a
DisasterMaster <mortenrechter@hotmail.com>
2015-05-08 21:02:47 +0200

    Added option for manual control

--- tests/simple_controller/simple.c
+++ tests/simple_controller/simple.c
@@ -99,10 +99,9 @@ void *simple_controller(void *arg){
     out = get_ctrlpad_x();
   }
     run_motory(get_ctrlpad_y());
-    run_motorx(out);
-#elif
-    run_motorx(out);
 #endif
+
+    run_motorx(out);
 
 
     rt_task_wait_period();
