930983ced340f216a56ca7f7109b42a5220c872d
Sgammeljord <mix_606_@msn.com>
2015-05-08 15:16:28 +0200

    sorry morten

--- matlab/control/PIDvalues.m
+++ matlab/control/PIDvalues.m
@@ -3,56 +3,23 @@ clear all;
 close;
 run('../model/cranemodel.m');
 %rlocus(Mx)
-Pvel = 0.15;
-pp = 1/(10+s);
+Pvel = 1;
+pp = 1/(20+s);
 PIv = Pvel * pp;
 dPIv = c2d(PIv, .01, 'tustin');
 loop3 = feedback(PIv * Mx, 1);
 X = Grx*rrx*(1/s);
 %rlocus(loop3*X)
-Ppos = 700; %P = 4.5;
+Ppos = 300; %P = 4.5;
 loop2 = feedback(Ppos * loop3 *  X, 1);
 %step(loop2*3.2)
-zp = 1*2;
-rlocus(loop2*-W*(zp + s)); grid on; %Cm = best gain with good damping
-Cm = 1.45; PDa = Cm * (zp + s);
+zp = .02;
+%rlocus(loop2*-W*(zp + s)); grid on; %Cm = best gain with good damping
+Cm = 1; PDa = Cm * (zp + s);
 %rlocus(loop2*-W); grid on;
-Pang = 2.79;
+Pang = 7;
 %loop1 = feedback(loop2 * -W, PDa);
 %step(loop1*3.2); grid on;
 %loop2 = feedback(P *  X, 1-W*PD);
 %loop1 = feedback(loop2 * W, PD);
 dPDa = c2d(PDa, .01, 'tustin');
-
-%% UDEN CONTAINER
-clear all;
-close;
-run('../model/cranemodel.m');
-X = Xu;
-W = Wu;
-
-%rlocus(X)
-P = 3.75;
-loop2 = feedback(P *  X, 1);
-%bode(loop2*-W) %zp = freq. at -45degrees
-zp = 4;
-preloop1 = feedback(loop2 * -W, (zp + s));
-%rlocus(loop2*-W*(zp + s)); grid on; %C2 = best gain with good damping
-Cm = 10;
-sv = 0.1;
-PD = Cm * (zp + sv*s);
-loop1 = feedback(loop2 * -W, PD);
-%step(loop2*3.2); grid on;
-%step(loop1*3.2); grid on;
-dPD = c2d(PD, .01, 'tustin');
-%% Y-AXIS
-%------------------------------------------------------------------------------
-% Y Control UPWARDS
-%------------------------------------------------------------------------------
-%rlocus(Yu) to find C3
-C3 = 49;
-%------------------------------------------------------------------------------
-% Y Control DOWNWARDS
-%------------------------------------------------------------------------------
-%rlocus(Yd) to find C4
-C4 = 94.9;
--- tests/simple_controller/simple.c
+++ tests/simple_controller/simple.c
@@ -39,11 +39,11 @@ void *simple_controller(void *arg){
   double pos_kp   = 2.5;
   double vel_kp   = 1;*/
     /*SG controller*/
-  double angle_kp = 1.45;
-  double angle_kd = 2;
-  double pos_kp   = 700;
-  double vel_kp   = 0.15;
-  double vel_ki   = 10;
+  double angle_kp = 1;
+  double angle_kd = .02;
+  double pos_kp   = 300;
+  double vel_kp   = 1;
+  double vel_ki   = 20;
 
   printf("REF: %lf\n", pos_ref);
 
@@ -55,7 +55,7 @@ void *simple_controller(void *arg){
 
     pos_out   = pos_kp * (pos_err - angle_out) ;
 
-    vel_err = pos_out - get_motorx_velocity();
+    vel_err = pos_out + get_motorx_velocity();
 
     out = vel_kp * vel_err + vel_ki * velocity_sum * SAMPLE_TIME_S;
     
@@ -65,7 +65,6 @@ void *simple_controller(void *arg){
     printf("pos_err     : %.3lf\n", pos_err);
     printf("pos_out     : %.3lf\n", pos_out);
     printf("velocity    : %.3lf\n", get_motorx_velocity());
-
     printf("out         : %.3lf\n", out);
     printf("===================\n");
 
