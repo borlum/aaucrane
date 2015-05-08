8e5133dd9a527284547bc9c9f4d6419433508c74
Joakim Børlum <joakim@roevhat.dk>
2015-05-08 12:59:03 +0200

    SLOWPOKE

--- tests/simple_controller/simple.c
+++ tests/simple_controller/simple.c
@@ -34,8 +34,8 @@ void *simple_controller(void *arg){
   
 
   double angle_kp = 1;
-  double pos_kp   = 1;
-  double vel_kp   = 1;
+  double pos_kp   = 2.5;
+  double vel_kp   = 2.5;
 
 
   printf("REF: %lf\n", pos_ref);
