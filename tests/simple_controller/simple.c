bcfeac3f625195f16709cda4107e238c31d4277f
Joakim Børlum <joakim@roevhat.dk>
2015-05-08 12:50:18 +0200

    Test tune

--- tests/simple_controller/simple.c
+++ tests/simple_controller/simple.c
@@ -33,9 +33,9 @@ void *simple_controller(void *arg){
   double prev_angle_err = 0;
   
 
-  double angle_kp = 1;
+  double angle_kp = 2.5;
   double pos_kp = 2.5;
-  double vel_kp = 5;
+  double vel_kp = 2.5;
 
 
   printf("REF: %lf\n", pos_ref);
