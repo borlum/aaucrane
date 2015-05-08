bcf61aa1466e3d10d615477535a02d60c114bafc
Sgammeljord <mix_606_@msn.com>
2015-05-08 12:31:59 +0200

    steffan is awesome

--- matlab/control/PIDvalues.m
+++ matlab/control/PIDvalues.m
@@ -2,29 +2,27 @@
 clear all;
 close;
 run('../model/cranemodel.m');
-Mx = Mxm;
 %rlocus(Mx)
 Pvel = 0.15;
 pp = 1/(10+s);
 PIv = Pvel * pp;
-dPIv = c2d(PIv, .001, 'tustin');
+dPIv = c2d(PIv, .01, 'tustin');
 loop3 = feedback(PIv * Mx, 1);
-X = gearx*(1/s);
+X = Grx*rrx*(1/s);
 %rlocus(loop3*X)
 Ppos = 700; %P = 4.5;
 loop2 = feedback(Ppos * loop3 *  X, 1);
-W = Wm;
 %step(loop2*3.2)
-zp = 4;
-%rlocus(loop2*-W*(zp + s)); grid on; %Cm = best gain with good damping
-%Cm = 1.22; PDa = Cm * (zp + s);
-rlocus(loop2*-W); grid on;
-Pang = 22.9;
-loop1 = feedback(loop2 * -W, PDa);
+zp = 1*2;
+rlocus(loop2*-W*(zp + s)); grid on; %Cm = best gain with good damping
+Cm = 1.45; PDa = Cm * (zp + s);
+%rlocus(loop2*-W); grid on;
+%Pang = 2.79;
+%loop1 = feedback(loop2 * -W, PDa);
 %step(loop1*3.2); grid on;
 %loop2 = feedback(P *  X, 1-W*PD);
 %loop1 = feedback(loop2 * W, PD);
-dPDa = c2d(PDa, .001, 'tustin');
+dPDa = c2d(PDa, .01, 'tustin');
 
 %% UDEN CONTAINER
 clear all;
