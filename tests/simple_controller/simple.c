f609eeb81ceb2d10631bc967e294a7352b5eb313
DisasterMaster <mortenrechter@hotmail.com>
2015-05-08 16:15:50 +0200

    vinkel gain på minus 1

--- tests/simple_controller/simple.c
+++ tests/simple_controller/simple.c
@@ -33,20 +33,18 @@ void *simple_controller(void *arg){
   double prev_angle_err = 0;
   double velocity_sum   = 0;
   
-  
+/*  
   double angle_kp = 2.45;
   double angle_kd = .02;
   double pos_kp   = 30;
   double vel_kp   = 1;
   double vel_ki   = 20;
-/*
-  double angle_kp = 4;
-  double angle_kd = 1;
-  double angle_k  = 3.46;
+*/
+  double angle_kp = 1;
+  double angle_kd = 0;
+  double angle_k  = 1;
   double pos_kp   = 2.5;
   double vel_kp   = 3;
-  double vel_ki   = 0;
-*/
 
   printf("REF: %lf\n", pos_ref);
 
@@ -54,15 +52,15 @@ void *simple_controller(void *arg){
     angle_err = angle_ref - get_angle();
     pos_err   = pos_ref   - get_xpos();
     
-//    angle_out = angle_k * ((angle_kp * angle_err) + (angle_kd * (angle_err - prev_angle_err) / SAMPLE_TIME_S));
-    angle_out = angle_kp * angle_err + (angle_kd * (angle_err - prev_angle_err) / SAMPLE_TIME_S);
+    angle_out = angle_k * ((angle_kp * angle_err) + (angle_kd * (angle_err - prev_angle_err) / SAMPLE_TIME_S));
+//    angle_out = angle_kp * angle_err + (angle_kd * (angle_err - prev_angle_err) / SAMPLE_TIME_S);
 
     pos_out   = pos_kp * (pos_err);
 
-    vel_err = pos_out - get_motorx_velocity();
-//    vel_err = pos_out - angle_out - get_x_velocity();
+//    vel_err = pos_out - get_motorx_velocity();
+    vel_err = pos_out - angle_out - get_x_velocity();
 
-    out = vel_kp * vel_err + vel_ki * velocity_sum * SAMPLE_TIME_S;
+    out = vel_kp * vel_err ; //+ vel_ki * velocity_sum * SAMPLE_TIME_S;
     
     printf("===================\n");
     printf("angle_err   : %.3lf\n", angle_err);
