d9b34e27d2a7f245890df73d4c26e5bb9f164ad3
Lasse Bromose <lbromo12@student.aau.dk>
2015-05-09 15:45:50 +0200

    hax on gain

--- acc/src/compensator.c
+++ acc/src/compensator.c
@@ -27,7 +27,7 @@ double angle_controller(double angle_err){
 }
 
 double position_controller_x(double error){
-  static double k_p = 1.6;
+  static double k_p = 1.4;
   int sign;
   if(fabs(error) < 0.10 && fabs(error) > 0.005){
     if(error < 0)
