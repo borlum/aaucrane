c203edaa7cd77e5186246a4a0eae1a304ae85fc7
Joakim Børlum <joakim@roevhat.dk>
2015-05-09 19:03:50 +0200

    No print

--- acc/src/compensator.c
+++ acc/src/compensator.c
@@ -71,12 +71,6 @@ double get_controller_output(double ref){
 
 
   out = velocity_controller_x(angle_out + pos_out - get_x_velocity());
-
-  printf("==================== \n");
-  printf("ANGLE CTRL. OUT: %lf \n", angle_out);
-  printf("POS   CTRL. OUT: %lf \n", pos_out);
-  printf("VEL   CTRL. OUT: %lf \n", out);
-  printf("==================== \n");
   
   return out;
 }
