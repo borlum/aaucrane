e9bdd8a5eb5beef250270eacd142acd2b6ecc2b6
Sgammeljord <mix_606_@msn.com>
2015-05-09 09:50:31 +0200

    PD PI

--- matlab/control/PIDvalues.m
+++ matlab/control/PIDvalues.m
@@ -2,18 +2,21 @@
 clear all;
 close;
 run('../model/cranemodel.m');
-%rlocus(Grx*rrx*Mx)
-Pvel = 3;
+%rlocus(Mx)
+Pvel = 11.6;
 %loop3 = feedback(Pvel * Grx*rrx*Mx, 1);
 %step(loop3*3.2);
 X = Mx*Grx*rrx*(1/s);
-%rlocus(X)
-Ppos = 9;
+%rlocus(X*1/(ip+s))
+Ppos = 7.5;
+ip = 2;
+PIp = Ppos + (1/(ip+s));
+dPIp = c2d(PIp, .01, 'tustin');
 loop2 = feedback(Ppos *  X, 1);
 %margin(loop2*3)
 zp = 2;
 %rlocus(loop2*-W*(zp + s)); grid on; %Cm = best gain with good damping
-Cm = 2; PDa = Cm * (zp + s);
+Cm = 3.6; PDa = Cm + (zp + s);
 %rlocus(loop2*-W*(4+s)); grid on;
 Pang = 9;
 %loop1 = feedback(loop2 * -W, Pang);
