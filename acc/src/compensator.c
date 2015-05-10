f814d530a4a547d380d84570039ac96bd9665ae9
Lasse Bromose <lbromo12@student.aau.dk>
2015-05-10 10:55:48 +0200

    less hack

--- acc/src/compensator.c
+++ acc/src/compensator.c
@@ -35,7 +35,8 @@ double angle_controller(double angle_err){
 }
 
 double position_controller_x(double error){
-  static double k_p = 2;
+  static double k_p = 1.2;
+  /*
   int sign;
   if(fabs(error) < 0.11 && fabs(error) > 0.005){
     if(error < 0)
@@ -44,6 +45,7 @@ double position_controller_x(double error){
       sign = 1;
     error = 0.10 * sign;
   }
+  */
   return error * k_p;
 }
 
