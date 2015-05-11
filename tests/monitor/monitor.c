21e3a270a97e334e1f785fa26dd3ce73903206e7
Joakim Børlum <joakim@roevhat.dk>
2015-05-11 12:31:21 +0200

    Lets monitor

--- tests/monitor/monitor.c
+++ tests/monitor/monitor.c
@@ -8,6 +8,7 @@ int main(int argc, char* argv[])
     
     while(1) {
         printf("(x,y,\u03B8) = (%.3lf, %.3lf, %.3lf)\n", get_xpos(), get_ypos(), get_angle());
+        printf("ANGLE RAW VOLTAGE = %lf \n", get_angle_raw());
         usleep(1000000 * .1);
     }
 }
