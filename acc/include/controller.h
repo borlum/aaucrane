ea7f1a8d84e6cc3aa821fa74c80d6b50f0ef5427
Joakim Børlum <joakim@roevhat.dk>
2015-05-14 10:46:32 +0200

    Less settle hits

--- acc/include/controller.h
+++ acc/include/controller.h
@@ -4,7 +4,7 @@
 #define X_ERR_BAND 0.005
 #define Y_ERR_BAND 0.005
 #define ANGLE_ERR_BAND 0.03
-#define SETTLE_HITS 100
+#define SETTLE_HITS 25
 
 void* task_x_axis_controller(void *);
 void* task_y_axis_controller(void *);
