e15142563012d1495a142bb4ae2af889e694cdc4
Joakim Børlum <joakim@roevhat.dk>
2015-05-14 14:38:45 +0200

    Monitor upd

--- tests/monitor/monitor.c
+++ tests/monitor/monitor.c
@@ -7,8 +7,14 @@ int main(int argc, char* argv[])
     initialize_crane();
     
     while(1) {
+
+        if (get_ctrlpad_magnet_switch()) {
+            enable_magnet();
+        } else {
+            disable_magnet();
+        }
+        
         printf("(x,y,\u03B8) = (%.3lf, %.3lf, %.3lf)\n", get_xpos(), get_ypos(), get_angle());
-        printf("ANGLE RAW VOLTAGE = %lf \n", get_angle_raw());
         usleep(1000000 * .1);
     }
 }
