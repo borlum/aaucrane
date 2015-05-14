4ada6a17c7135ce64493d71342db76b1f6881abe
Lasse Bromose <lbromo12@student.aau.dk>
2015-05-14 15:39:48 +0200

    dont do that

--- tests/monitor/monitor.c
+++ tests/monitor/monitor.c
@@ -8,13 +8,7 @@ int main(int argc, char* argv[])
     
     while(1) {
 
-        if (get_ctrlpad_magnet_switch()) {
-            enable_magnet();
-        } else {
-            disable_magnet();
-        }
-        
-        printf("(x,y,\u03B8) = (%.3lf, %.3lf, %.3lf)\n", get_xpos(), get_ypos(), get_angle());
+       printf("(x,y,\u03B8) = (%.3lf, %.3lf, %.3lf)\n", get_xpos(), get_ypos(), get_angle());
         usleep(1000000 * .1);
     }
 }
