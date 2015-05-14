d223e50a9e3231e3a00615e4d4f0b435b94fa0e8
Joakim Børlum <joakim@roevhat.dk>
2015-05-14 15:45:30 +0200

    Monitor, begge vinkler

--- tests/monitor/monitor.c
+++ tests/monitor/monitor.c
@@ -8,4 +8,5 @@ int main(int argc, char* argv[])
     
     while(1) {
     printf("(x,y,\u03B8 uden,\u03B8 med) = (%.3lf, %.3lf, %.3lf, %.3lf)\n", get_xpos(), get_ypos(), get_angle(), get_angle() + 0.021);
+    }
 }
