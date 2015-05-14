e13945644ce3e921cbb2428b5fb7f1b38cdd8a58
Joakim Børlum <joakim@roevhat.dk>
2015-05-14 20:27:02 +0200

    Monitor update

--- tests/monitor/monitor.c
+++ tests/monitor/monitor.c
@@ -10,4 +10,7 @@ int main(int argc, char* argv[])
     printf("\033[2J");
 
     printf("x: %+.3lf [m] | y: %+.3lf [m] | \u03B8 (u): %+.3lf [rad] | \u03B8 (m): %+.3lf [rad] | U: %+.3lf [V]\n", get_xpos(), get_ypos(), get_angle(), get_angle() + 0.021, get_motorx_voltage());
+
+    usleep(1000000 * 0.01);
+    }
 }
