5fe7e47a10f4f1e24f566819af780ab6427401ab
Sgammeljord <mix_606_@msn.com>
2015-05-16 16:14:04 +0200

    y control

--- matlab/control/ycontroller.m
+++ matlab/control/ycontroller.m
@@ -18,31 +18,31 @@ omega_d = 4.6/(Ts*Sigma_d); % = 1.1500
 omega = [omega_d omega_u]
 %margin(Hywd); = GM = inf, PM = 89.3
 %rlocus(Hywd);
-Pywd = 26; %WITH CONTAINER DOWN
+Pywd = 26.9; %WITH CONTAINER DOWN
 wcd = bandwidth(feedback(Pywd*Hywd, 1));
-step(feedback(Hywd*Pywd, 1)*0.78); grid on; xlabel('t'); ylabel('y[m]'); title('Step response of y-axis @ 0.78'); xlim([0 7]); stepinfo(feedback(Hywd*Pywd, 1)*0.78)
-%hold off; plot(WCD1); hold on; grid on; plot(WCD);
+%step(feedback(Hywd*Pywd, 1)*0.78); grid on; xlabel('t'); ylabel('y[m]'); title('Step response of y-axis @ 0.78'); xlim([0 7]); stepinfo(feedback(Hywd*Pywd, 1)*0.78)
+%hold off; plot(WCD); grid on; xlabel('t[seconds]'); ylabel('y[m]'); title('Zoomed step response of y-axis @ 0.78'); hold on; plot([0 7],[0.778 0.778],'k--'); plot([0 7],[0.782 0.782],'k--')
 
 %margin(Hywu); = GM = inf, PM = 89.7
 %rlocus(Hywu);
-Pywu = 75.9; %WITH CONTAINER UP
+Pywu = 125; %WITH CONTAINER UP
 wcu = bandwidth(feedback(Pywu*Hywu, 1));
 %step(feedback(Pywu*Hywu, 1)*0.78); grid on; xlabel('t'); ylabel('y[m]'); title('Step response of y-axis @ 0.78'); xlim([0 7]); stepinfo(feedback(Pywu*Hywu, 1)*0.78)
-%hold off; plot(WCU1); hold on; grid on; plot(WCU);
+%hold off; plot(WCU); grid on; xlabel('t[seconds]'); ylabel('y[m]'); title('Zoomed step response of y-axis @ -0.78'); hold on; plot([0 10],[-0.778 -0.778],'k--'); plot([0 10],[-0.782 -0.782],'k--')
 
 %margin(Hynd); = GM = inf, PM = 89.6
 %rlocus(Hynd);
-Pynd = 38.5; %NO CONTAINER DOWN
+Pynd = 55; %NO CONTAINER DOWN
 ncd = bandwidth(feedback(Pynd*Hynd, 1));
 %step(feedback(Pynd*Hynd, 1)*0.78); grid on; xlabel('t'); ylabel('y[m]'); title('Step response of y-axis @ 0.78'); xlim([0 7]); stepinfo(feedback(Pynd*Hynd, 1)*0.78)
-%hold off; plot(NCD1); hold on; grid on; plot(NCD);
+%hold off; plot(NCD); grid on; xlabel('t[seconds]'); ylabel('y[m]'); title('Zoomed step response of y-axis @ 0.78'); hold on; plot([0 10],[0.778 0.778],'k--'); plot([0 10],[0.782 0.782],'k--')
 
 %margin(Hynu); = GM = inf, PM = 89.5
 %rlocus(Hynu);
-Pynu = 59.6; %NO CONTAINER UP
+Pynu = 44; %NO CONTAINER UP
 ncu = bandwidth(feedback(Pynu*Hynu, 1));
 %step(feedback(Pynu*Hynu, 1)*0.78); grid on; xlabel('t'); ylabel('y[m]'); title('Step response of y-axis @ 0.78'); xlim([0 7]); stepinfo(feedback(Pynu*Hynu, 1)*0.78)
-%hold off; plot(NCU1); hold on; grid on; plot(NCU);
+%hold off; plot(NCU); grid on; xlabel('t[seconds]'); ylabel('y[m]'); title('Zoomed step response of y-axis @ -0.78'); hold on; plot([0 8],[-0.778 -0.778],'k--'); plot([0 8],[-0.782 -0.782],'k--')
 
 BWwc = [wcd wcu]
 BWnc = [ncd ncu]
