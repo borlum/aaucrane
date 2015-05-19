a9a8f29a6f87dbf808ab0ee942676ef27e0e06ea
Joakim Børlum <joakim@roevhat.dk>
2015-05-19 19:57:21 +0200

    OFFSET FUCK

--- libcrane/libcrane.c
+++ libcrane/libcrane.c
@@ -6,7 +6,7 @@ comedi_t *NI_card;
 int *NI_card;
 #endif
 
-#define ANGLE_OFFSET 0.499
+#define ANGLE_OFFSET 0.541
 
 static const double MAX_MOTOR_OUTPUT = 12.5;
 static const double MIN_MOTOR_OUTPUT = 0;
