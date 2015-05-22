4dec5c3136ad0ce36592f6aca2c22e12162fd394
Lasse Bromose <lbromo12@student.aau.dk>
2015-05-22 09:37:19 +0200

    reset stuff

--- acc/acc.c
+++ acc/acc.c
@@ -38,6 +38,8 @@ void *controller(void * args)
   size_t cmd_buf_len = BUFFER_SIZE;
   char cmd_buf[BUFFER_SIZE];
 
+  int tmp = 0;
+
   to_x = mq_open(Q_TO_X, O_WRONLY);
   from_x = mq_open(Q_FROM_X, O_RDONLY);
 
@@ -51,9 +53,9 @@ void *controller(void * args)
   pthread_create(&thread_xcontroller, NULL, task_x_axis_controller, NULL); /* Starts with a ref @ 0 */
   pthread_create(&thread_ycontroller, NULL, task_y_axis_controller, NULL);  /* Starts with a ref @ 0 */
   /* Wait untill (0,0) has been reached */
-  mq_send(to_y, (char*) &reset_pos_flag, sizeof(double), 0);
+  mq_send(to_y, (char*) &tmp, sizeof(double), 0);
   mq_receive(from_y, buf, buf_len, 0);
-  mq_send(to_x, (char*) &reset_pos_flag, sizeof(double), 0);
+  mq_send(to_x, (char*) &tmp, sizeof(double), 0);
   mq_receive(from_x, buf, buf_len, 0);
   
   while(1){
